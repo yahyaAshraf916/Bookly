13fa08a32bb65b7940ea604078d986eaf9c8f76a
yahyaAshraf916 <yahyaashraf916@gmail.com>
2023-09-15 05:09:19 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:books_app/feature/splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(const Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home:SplashView()
+    );
+  }
+}
